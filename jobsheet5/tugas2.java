14ecbeaf1b4b994b9ae99b4af088ab94d8fd9e80
rayyanabie5-lgtm <rayyanabie5@gmail.com>
2025-10-01 14:51:09 +0700

    tugas 2

---
+++ jobsheet5/tugas2.java
@@ -0,0 +1,23 @@
+package jobsheet5;
+import java.util.Scanner;
+
+public class tugas2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Program Pengecekan Ganjil Genap ---");
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
+
+        sc.close();
+    }
+}
+    
+    
+
