de377de7b2cd9a36b056c1256ec93ee758d9214f
rayyanabie5-lgtm <rayyanabie5@gmail.com>
2025-10-01 12:15:57 +0700

    percobaan 1

--- jobsheet5/ifCetakKRS20.java
+++ jobsheet5/ifCetakKRS20.java
@@ -15,14 +15,10 @@ public class ifCetakKRS20 {
         System.out.println("Pembayaran UKT terverifikasi");
         System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
 
+    } else {
+        System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
     }
-
         sc.close();
     }
     
-
-
-    
-
-
 }
